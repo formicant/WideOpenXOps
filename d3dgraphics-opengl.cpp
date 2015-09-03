b001a755ff95a47e75ce2336bbcc89841caf29d7
xops-mikan <mikan@openxops.net>
2015-09-03 15:07:37 +0000

    ライセンス文を細部編集
    git-svn-id: svn://svn.pf.osdn.net/x/xo/xops-mikan/OpenXOPS/trunk@105 ff51d050-6dfb-4b14-850f-860e8d883e2a

--- d3dgraphics-opengl.cpp
+++ d3dgraphics-opengl.cpp
@@ -13,7 +13,7 @@
 // * Redistributions in binary form must reproduce the above copyright notice, 
 //   this list of conditions and the following disclaimer in the documentation 
 //   and/or other materials provided with the distribution.
-// * Neither the name of the OpenXOPS Project nor the　names of its contributors 
+// * Neither the name of the OpenXOPS Project nor the names of its contributors 
 //   may be used to endorse or promote products derived from this software 
 //   without specific prior written permission.
 // 
@@ -1732,6 +1732,7 @@ void D3DGraphics::Draw2DMSFontText(int x, int y, char *str, int color)
 
 		//ディスプレイリストを作成
 		listIdx = glGenLists(len);
+		wglUseFontBitmapsW(hDC, ustr[0], 1, listIdx);	//ダミー
 		for(int i=0; i<lstrlenW(ustr); i++){
 			wglUseFontBitmapsW(hDC, ustr[i], 1, listIdx+i);
 		}
